e4bb312a7e9af8fe6fd0fd739208169d65f71bf1
Ellen Probst <eprobst@twg.ca>
2020-05-18 13:04:41 -0400

    small update

--- src/components/hero/rectangle.js
+++ src/components/hero/rectangle.js
@@ -1,18 +1,12 @@
-import React from "react"
+import React, { useMemo } from "react"
 import { useUpdate } from "react-three-fiber"
 import * as THREE from "three"
 import { animated } from "react-spring/three"
 
-const Rectangle = props => {
-  function getPoints() {
-    const recShape = getRoundedRect(0, 0, 2.7, 2.7, 1)
-    const points = recShape.getPoints()
-    return points
-  }
-
-  function getRoundedRect(x, y, width, height, radius) {
+const Rectangle = ({ x = 0, y = 0, width = 2.7, height = 2.7, ...props }) => {
+  const getRoundedRect = useMemo(() => {
     const ctx = new THREE.Shape()
-
+    const radius = 1
     ctx.moveTo(x, y + radius)
     ctx.lineTo(x, y + height - radius)
     ctx.quadraticCurveTo(x, y + height, x + radius, y + height)
@@ -24,14 +18,14 @@ const Rectangle = props => {
     ctx.quadraticCurveTo(x, y, x, y + radius)
 
     return ctx
-  }
+  }, [x, y, width, height])
 
   const ref = useUpdate(
     geometry => {
-      geometry.setFromPoints(getPoints())
+      geometry.setFromPoints(getRoundedRect.getPoints())
       geometry.center()
     },
-    [getPoints]
+    [getRoundedRect]
   )
   const color = `hsl(323, 56%, ${50 - Math.round(props.brightness)}%)`
 
