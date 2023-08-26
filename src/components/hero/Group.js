3673823b478575cf605189f8eab16219ba44d590
Ellen Probst <ellenprobst@hotmail.com>
2023-08-26 13:39:54 -0400

    speed up animation

--- src/components/hero/Group.js
+++ src/components/hero/Group.js
@@ -7,7 +7,7 @@ const fast = { tension: 1200, friction: 40 }
 const slow = { mass: 1, tension: 800, friction: 160 }
 
 const enter = i => ({
-  z: i * 0.08,
+  z: i * 0.05,
 })
 const from = i => ({
   z: -Math.pow(i * 0.45, 2),
